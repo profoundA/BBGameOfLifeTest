99cb96ab29e0e710e1b9791268ed986c5001ed57
profoundA <81711479+profoundA@users.noreply.github.com>
2022-08-09 14:39:55 +0300

    Initial Commit

---
+++ BBGameOfLifeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BBGameOfLifeTest
+//
+//  Created by Andrey Lobanov on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
